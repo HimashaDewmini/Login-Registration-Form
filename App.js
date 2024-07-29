d51234d2c15640107cfaa54d77ab67d58e689ccf
Himasha Dewmini <himashadewmini12345@gmail.com>
2024-07-29 16:22:41 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import LoginRegister from './components/loginRegister/loginRegister';
+
+
+function App() {
+  return (
+    <div>
+      <LoginRegister/>
+    </div>
+  );
+}
+
+export default App;
